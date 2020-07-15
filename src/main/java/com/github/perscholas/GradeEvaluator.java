3590a412c37985264a373cbea51f05097511661b
Ghassan Nasr <ghassan.nasr@gmail.com>
2020-07-15 10:32:41 -0400

    assignment completed

--- src/main/java/com/github/perscholas/GradeEvaluator.java
+++ src/main/java/com/github/perscholas/GradeEvaluator.java
@@ -5,26 +5,35 @@ package com.github.perscholas;
  */
 public class GradeEvaluator {
     public Boolean isGradeA(Double score) {
-        return null;
+        return score >= 90 && score <= 100;
     }
 
     public Boolean isGradeB(Double score) {
-        return null;
+        return score >= 80 && score <= 89;
     }
 
     public Boolean isGradeC(Double score) {
-        return null;
+        return score >= 70 && score <= 79;
     }
 
     public Boolean isGradeD(Double score) {
-        return null;
+        return score >= 65 && score <= 69;
     }
 
     public Boolean isGradeF(Double score) {
-        return null;
+        return score <= 64;
     }
 
     public Character getGrade(Double score) {
-        return null;
+        if (isGradeA(score))
+            return 'A';
+        else if (isGradeB(score))
+            return 'B';
+        else if (isGradeC(score))
+            return 'C';
+        else if (isGradeD(score))
+            return 'D';
+        else
+            return 'F';
     }
 }
